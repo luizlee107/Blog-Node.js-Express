9e1feacc98a3505e8c46da4b8e953e74dfb92b21
luizlee107 <luiz014@hotmail.com>
2023-01-30 02:13:48 -0300

    add category

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = require('../src/app');
-const { render } = require('../src/app');
+const render = require('../src/app');
 const router = express.Router();
 
 
 router.get('/', (req,res) => {
-    return res.render('../views/layouts/main');
+    return res.render('../views/home');
 });
 
 
